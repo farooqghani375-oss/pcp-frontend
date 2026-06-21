b8420d20e7557555f0a00c1bb7437eeb0ee8d9b7
Abubakkar <farooqghani375@gmail.com>
2026-06-21 09:07:49 +0500

    Fix shop page suspense boundary for build

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,8 +1,13 @@
+import { Suspense } from 'react'
 import { getProducts } from '@/lib/api'
 import ShopClient from './ShopClient'
 
 export default async function ShopPage() {
   let products = []
   try { products = await getProducts() } catch (e) {}
-  return <ShopClient products={products} />
+  return (
+    <Suspense fallback={<div className="pt-20 text-center text-gray-400">Loading...</div>}>
+      <ShopClient products={products} />
+    </Suspense>
+  )
 }
